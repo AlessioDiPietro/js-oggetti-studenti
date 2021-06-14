a90ff4a9da4d46c39c1e551582bb8e8f0cb37deb
AlessioDPT <alessio.recos@gmail.com>
2021-06-14 16:21:48 +0200

    ADD- STEP 4

--- js/script.js
+++ js/script.js
@@ -25,12 +25,12 @@ var listaStudenti = [
         nome : "Luke",
         cognome : "Skywalker",
         age : 20,
-    }
+    },
     {
         nome : "Tony",
         cognome : "Stark",
         age : 24,
-    }
+    },
     {
         nome : "Bilbo",
         cognome : "Baggins",
@@ -38,4 +38,9 @@ var listaStudenti = [
     }
 ];
 
-// Ciclare su tutti gli studenti e stampare per ognuno di essi, nome e cognome.
+//* Ciclare su tutti gli studenti e stampare per ognuno di essi, nome e cognome.
+for (var i = 0; i < listaStudenti.length; i++){
+    
+        document.getElementById("lista_studenti_nome").innerHTML += listaStudenti[i]["nome"] + " ";
+        document.getElementById("lista_studenti_cognome").innerHTML += listaStudenti[i]["cognome"] + " ";
+}
