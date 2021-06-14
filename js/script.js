3f44505217940134593ced88876e34860bbca04d
AlessioDPT <alessio.recos@gmail.com>
2021-06-14 16:00:50 +0200

    ADD- STEP 2 & 3

--- js/script.js
+++ js/script.js
@@ -5,8 +5,37 @@
 // Dare la possibilità all’utente, attraverso 3 prompt(), di aggiungere un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età.
 
 
+//* Creare un oggetto che descriva uno studente, con le seguenti proprietà: nome, cognome e età.
 var infoStudente = {
     nome : "Luke",
     cognome : "Skywalker",
     age : 20,
+};
+
+//* Stampare a schermo attraverso un ciclo for-in tutte le proprietà dell'oggetto.
+for (var key in infoStudente){
+    document.getElementById("lista_proprieta").innerHTML += infoStudente[key] + " ";
 }
+
+
+//* Creare un array di oggetti di studenti.
+
+var listaStudenti = [
+    {
+        nome : "Luke",
+        cognome : "Skywalker",
+        age : 20,
+    }
+    {
+        nome : "Tony",
+        cognome : "Stark",
+        age : 24,
+    }
+    {
+        nome : "Bilbo",
+        cognome : "Baggins",
+        age : 22,
+    }
+];
+
+// Ciclare su tutti gli studenti e stampare per ognuno di essi, nome e cognome.
